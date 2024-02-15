141836713233e2bf55433fb83932fd1a6d7cfb19
Viviana Molina <viviana.a.molina@gmail.com>
2024-02-14 21:26:21 -0300

    Cambia el calculo del factorial por un ciclo anidado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,28 @@
 var num = prompt("Ingrese un numero entero menor a 21");
 var resultado = 0;
 
-if (num > 20){
-    alert("El valor debe ser un numero entero menor a 21");
-}
-else
-{
-    var factorial = 1;
-    for (let i = 1; i <= num; i++) {
-        multiplicar(num, i);
-        factorial = factorial * i;
-    }
-    console.log(`El factorial de ${num} es ${factorial}`);
-    document.write(`El factorial de ${num} es ${factorial}</p>`);
-}
-
-let multiplicar = (numero, i) => {
+let CalcularMultiplicacion = (numero, i) => {
     resultado = numero * i;
     console.log(`${numero} * ${i} = ${resultado}`);
     document.write(`<p>${numero} * ${i} = ${resultado}</p>`);
    return resultado;
+}
+
+if (num < 1 || num > 20){
+    alert("número fuera del rango");
+}
+else
+{
+    for (let i = 1; i <= num; i++) {
+        CalcularMultiplicacion(num, i);
+    }
+    
+    for (let i = 1; i <= num; i++) {
+        var factorial = 1;
+        for (let j = 1; j <= num; j++) { 
+            factorial = factorial * j;
+        }
+    }
+    console.log(`El factorial de ${num} es ${factorial}`);
+    document.write(`El factorial de ${num} es ${factorial}</p>`);
 }
